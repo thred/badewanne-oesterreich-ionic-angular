959342c5e2878a31067c3044d756266b653d50d7
Manfred Hantschel <manfred.hantschel@porscheinformatik.com>
2023-07-14 11:41:33 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Badewanne Österreich Ionic Angular',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
